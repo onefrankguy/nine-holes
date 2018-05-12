3b92db3c27dab2f3dd56cc2d03d8c19bf240eb36
Frank Mitchell <me@frankmitchell.org>
2018-05-12 17:40:08 -0400

    Add a basic render loop to the game.

--- js/game.js
+++ js/game.js
@@ -1,5 +1,28 @@
+const Renderer = (function renderer() {
+  let dirty = true;
+
+  function render() {
+    if (dirty) {
+      dirty = false;
+    }
+
+    requestAnimationFrame(render);
+  }
+
+  function invalidate() {
+    dirty = true;
+  }
+
+  return {
+    render,
+    invalidate,
+  };
+}());
+
 const Game = (function game() {
   function play() {
+    Renderer.invalidate();
+    Renderer.render();
   }
 
   return {
